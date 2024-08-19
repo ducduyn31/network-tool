99af6ef1b87e936c87a8ab2dfcf3f6c9a15c9b35
ducduyn31 <ducduy.ababat@gmail.com>
2024-08-20 08:49:52 +1000

    fix: pcap writer queue empty

--- network_activity_stream/packet_sniffer.py
+++ network_activity_stream/packet_sniffer.py
@@ -1,6 +1,6 @@
 from typing import Set
 
-from network_activity_stream.shared_state import network_activity_state
+from network_activity_stream.shared_state import network_activity_state, packet_queue
 
 
 def sniff_callback_builder(source_ips: Set[str]):
@@ -8,9 +8,11 @@ def sniff_callback_builder(source_ips: Set[str]):
         packet_size = len(packet)
 
         # Update the shared state
+        packet_queue.put(packet)
         if packet.haslayer("IP"):
             ip_layer = packet["IP"]
 
+            # Check if the packet is sent or received
             if ip_layer.src in source_ips:
                 network_activity_state.last_second_sent_size += packet_size
             elif ip_layer.dst in source_ips:
